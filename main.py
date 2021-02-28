9b91ba87f7890eda4c2ec10d0d3a7c6d61dca408
Sarveshprasath <74827042+Sarveshprasath@users.noreply.github.com>
2021-02-28 13:40:29 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,311 @@
+var = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25,26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99]
+
+print("Welcome to Hacker fun")
+
+answe1 = input("Which type of Hacker you are: ")
+if answe1 == "White hat hacker" :
+	answer = input("Oh You are a " + answe1  + "?")	
+	if answer == "yes": 
+		answer4 = input("Your Name: ")
+		if answer4 == answer4:
+			answer = input("What programming language have you learned?: ")
+			if answer == "Python":
+				answer = input(answer +" is Good language " + answer4)
+			elif answer == "A.NET":
+				answer = input(answer +" is Good language " + answer4)
+			elif answer == "A-O":
+				answer = input(answer +" is Good language " + answer4)
+			elif answer == "A+":
+				answer = input(answer +" is Good language " + answer4)
+			elif answer == "ABAP":
+				answer = input(answer +" is Good language " + answer4)
+			elif answer == "C":
+				answer = input(answer +" is Good language " + answer4)
+			elif answer == "C++":
+				answer = input(answer +" is Good language " + answer4)
+			elif answer == "C#":
+				answer = input(answer +" is Good language " + answer4)
+			elif answer == "Cython":
+				answer = input(answer +" is Good language " + answer4)
+			elif answer == "Dart":
+				answer = input(answer +" is Good language " + answer4)
+			elif answer == "Erlang":
+				answer = input(answer +" is Good language " + answer4)
+			elif answer == "Java":
+				answer = input(answer +" is Good language " + answer4)
+			elif answer == "Javascript":
+				answer = input(answer +" is Good language " + answer4)
+			elif answer == "Kotlin":
+				answer = input(answer +" is Good language " + answer4)
+			elif answer == "PHP":
+				answer = input(answer +" is Good language " + answer4)
+			elif answer == "Perl":
+				answer = input(answer +" is Good language " + answer4)
+			elif answer == "typescript":
+				answer = input(answer +" is Good language " + answer4)
+			elif answer == "Basic":
+				answer = input(answer +" is Good language " + answer4)
+			elif answer == "Golang":
+				answer = input(answer +" is Good language " + answer4)
+			
+			else:
+				print("No programming language named as " + answer)
+	else: 
+		print("Game Over!!!")
+#=======================================================================
+
+elif answe1 == "Black hat hacker":
+	answer = input("Oh You are a " + answe1  + "?")
+	if answer == "yes":
+		answer4 = input("Your Name: ")
+		if answer4 == answer4:
+			answer = input("What programming language have you learned?: ")
+			if answer == "Python":
+				answer = input(answer +" is Good language " + answer4)
+			elif answer == "A.NET":
+				answer = input(answer +" is Good language " + answer4)
+			elif answer == "A-O":
+				answer = input(answer +" is Good language " + answer4)
+			elif answer == "A+":
+				answer = input(answer +" is Good language " + answer4)
+			elif answer == "ABAP":
+				answer = input(answer +" is Good language " + answer4)
+			elif answer == "C":
+				answer = input(answer +" is Good language " + answer4)
+			elif answer == "C++":
+				answer = input(answer +" is Good language " + answer4)
+			elif answer == "C#":
+				answer = input(answer +" is Good language " + answer4)
+			elif answer == "Cython":
+				answer = input(answer +" is Good language " + answer4)
+			elif answer == "Dart":
+				answer = input(answer +" is Good language " + answer4)
+			elif answer == "Erlang":
+				answer = input(answer +" is Good language " + answer4)
+			elif answer == "Java":
+				answer = input(answer +" is Good language " + answer4)
+			elif answer == "Javascript":
+				answer = input(answer +" is Good language " + answer4)
+			elif answer == "Kotlin":
+				answer = input(answer +" is Good language " + answer4)
+			elif answer == "PHP":
+				answer = input(answer +" is Good language " + answer4)
+			elif answer == "Perl":
+				answer = input(answer +" is Good language " + answer4)
+			elif answer == "typescript":
+				answer = input(answer +" is Good language " + answer4)
+			elif answer == "Basic":
+				answer = input(answer +" is Good language " + answer4)
+			elif answer == "Golang":
+				answer = input(answer +" is Good language " + answer4)
+			else:
+				print("No programming language named as " + answer)
+	else: 
+		print("Game Over!!!")
+
+#=======================================================================
+
+elif answe1 == "black hat hacker":
+	answer = input("Oh You are a " + answe1  + "?")
+	if answer == "yes":
+		answer4 = input("Your Name: ")
+
+		if answer4 == answer4:
+			answer = input("What programming language have you learned?: ")
+			if answer == "Python":
+				answer = input(answer +" is Good language " + answer4)
+			elif answer == "A.NET":
+				answer = input(answer +" is Good language " + answer4)
+			elif answer == "A-O":
+				answer = input(answer +" is Good language " + answer4)
+			elif answer == "A+":
+				answer = input(answer +" is Good language " + answer4)
+			elif answer == "ABAP":
+				answer = input(answer +" is Good language " + answer4)
+			elif answer == "C":
+				answer = input(answer +" is Good language " + answer4)
+			elif answer == "C++":
+				answer = input(answer +" is Good language " + answer4)
+			elif answer == "C#":
+				answer = input(answer +" is Good language " + answer4)
+			elif answer == "Cython":
+				answer = input(answer +" is Good language " + answer4)
+			elif answer == "Dart":
+				answer = input(answer +" is Good language " + answer4)
+			elif answer == "Erlang":
+				answer = input(answer +" is Good language " + answer4)
+			elif answer == "Java":
+				answer = input(answer +" is Good language " + answer4)
+			elif answer == "Javascript":
+				answer = input(answer +" is Good language " + answer4)
+			elif answer == "Kotlin":
+				answer = input(answer +" is Good language " + answer4)
+			elif answer == "PHP":
+				answer = input(answer +" is Good language " + answer4)
+			elif answer == "Perl":
+				answer = input(answer +" is Good language " + answer4)
+			elif answer == "typescript":
+				answer = input(answer +" is Good language " + answer4)
+			elif answer == "Basic":
+				answer = input(answer +" is Good language " + answer4)
+			elif answer == "Golang":
+				answer = input(answer +" is Good language " + answer4)
+			else:
+				print("No programming language named as " + answer)
+	else: 
+		print("Game Over!!!")
+#=======================================================================
+
+elif answe1 == "white hat hacker":
+	answer = input("Oh You are a " + answe1  + "?")
+	if answer == "yes":
+		answer4 = input("Your Name: ")
+		if answer4 == answer4:
+			answer = input("What programming language have you learned?: ")
+			if answer == "Python":
+				answer = input(answer +" is Good language " + answer4)
+			elif answer == "A.NET":
+				answer = input(answer +" is Good language " + answer4)
+			elif answer == "A-O":
+				answer = input(answer +" is Good language " + answer4)
+			elif answer == "A+":
+				answer = input(answer +" is Good language " + answer4)
+			elif answer == "ABAP":
+				answer = input(answer +" is Good language " + answer4)
+			elif answer == "C":
+				answer = input(answer +" is Good language " + answer4)
+			elif answer == "C++":
+				answer = input(answer +" is Good language " + answer4)
+			elif answer == "C#":
+				answer = input(answer +" is Good language " + answer4)
+			elif answer == "Cython":
+				answer = input(answer +" is Good language " + answer4)
+			elif answer == "Dart":
+				answer = input(answer +" is Good language " + answer4)
+			elif answer == "Erlang":
+				answer = input(answer +" is Good language " + answer4)
+			elif answer == "Java":
+				answer = input(answer +" is Good language " + answer4)
+			elif answer == "Javascript":
+				answer = input(answer +" is Good language " + answer4)
+			elif answer == "Kotlin":
+				answer = input(answer +" is Good language " + answer4)
+			elif answer == "PHP":
+				answer = input(answer +" is Good language " + answer4)
+			elif answer == "Perl":
+				answer = input(answer +" is Good language " + answer4)
+			elif answer == "typescript":
+				answer = input(answer +" is Good language " + answer4)
+			elif answer == "Basic":
+				answer = input(answer +" is Good language " + answer4)
+			elif answer == "Golang":
+				answer = input(answer +" is Good language " + answer4)
+			else:
+				print("No programming language named as " + answer)
+	else: 
+		print("Game Over!!!")
+#=======================================================================
+
+elif answe1 == "Grey hat hacker":
+	answer = input("Oh You are a " + answe1  + "?")
+	if answer == "yes":
+		answer4 = input("Your Name: ")
+		if answer4 == answer4:
+			answer = input("What programming language have you learned?: ")
+			if answer == "Python":
+				answer = input(answer +" is Good language " + answer4)
+			elif answer == "A.NET":
+				answer = input(answer +" is Good language " + answer4)
+			elif answer == "A-O":
+				answer = input(answer +" is Good language " + answer4)
+			elif answer == "A+":
+				answer = input(answer +" is Good language " + answer4)
+			elif answer == "ABAP":
+				answer = input(answer +" is Good language " + answer4)
+			elif answer == "C":
+				answer = input(answer +" is Good language " + answer4)
+			elif answer == "C++":
+				answer = input(answer +" is Good language " + answer4)
+			elif answer == "C#":
+				answer = input(answer +" is Good language " + answer4)
+			elif answer == "Cython":
+				answer = input(answer +" is Good language " + answer4)
+			elif answer == "Dart":
+				answer = input(answer +" is Good language " + answer4)
+			elif answer == "Erlang":
+				answer = input(answer +" is Good language " + answer4)
+			elif answer == "Java":
+				answer = input(answer +" is Good language " + answer4)
+			elif answer == "Javascript":
+				answer = input(answer +" is Good language " + answer4)
+			elif answer == "Kotlin":
+				answer = input(answer +" is Good language " + answer4)
+			elif answer == "PHP":
+				answer = input(answer +" is Good language " + answer4)
+			elif answer == "Perl":
+				answer = input(answer +" is Good language " + answer4)
+			elif answer == "typescript":
+				answer = input(answer +" is Good language " + answer4)
+			elif answer == "Basic":
+				answer = input(answer +" is Good language " + answer4)
+			elif answer == "Golang":
+				answer = input(answer +" is Good language " + answer4)
+			else:
+				print("No programming language named as " + answer)
+	else: 
+		print("Game Over!!!")
+#=======================================================================
+
+elif answe1 == "grey hat hacker":
+	answer = input("Oh You are a " + answe1  + "?")
+	if answer == "yes":
+		answer4 = input("Your Name: ")
+		if answer4 == answer4:
+			answer = input("What programming language have you learned?: ")
+			if answer == "Python":
+				answer = input(answer +" is Good language " + answer4)
+			elif answer == "A.NET":
+				answer = input(answer +" is Good language " + answer4)
+			elif answer == "A-O":
+				answer = input(answer +" is Good language " + answer4)
+			elif answer == "A+":
+				answer = input(answer +" is Good language " + answer4)
+			elif answer == "ABAP":
+				answer = input(answer +" is Good language " + answer4)
+			elif answer == "C":
+				answer = input(answer +" is Good language " + answer4)
+			elif answer == "C++":
+				answer = input(answer +" is Good language " + answer4)
+			elif answer == "C#":
+				answer = input(answer +" is Good language " + answer4)
+			elif answer == "Cython":
+				answer = input(answer +" is Good language " + answer4)
+			elif answer == "Dart":
+				answer = input(answer +" is Good language " + answer4)
+			elif answer == "Erlang":
+				answer = input(answer +" is Good language " + answer4)
+			elif answer == "Java":
+				answer = input(answer +" is Good language " + answer4)
+			elif answer == "Javascript":
+				answer = input(answer +" is Good language " + answer4)
+			elif answer == "Kotlin":
+				answer = input(answer +" is Good language " + answer4)
+			elif answer == "PHP":
+				answer = input(answer +" is Good language " + answer4)
+			elif answer == "Perl":
+				answer = input(answer +" is Good language " + answer4)
+			elif answer == "typescript":
+				answer = input(answer +" is Good language " + answer4)
+			elif answer == "Basic":
+				answer = input(answer +" is Good language " + answer4)
+			elif answer == "Golang":
+				answer = input(answer +" is Good language " + answer4)
+			else:
+				print("No programming language named as " + answer)
+	else: 
+		print("Game Over!!!")
+#=======================================================================
+
+else:
+	print("Sorry! No Hacker named as " + answe1)
+#=======================================================================
